b3d09a4261fedb958c92af75956ccb0a59c69127
Lily <91525799+Lilyb03@users.noreply.github.com>
2024-03-09 20:47:35 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,59 @@ function darkMode(){
 		darkModeOn = true;
 	}
 }
+
+function percDiff(stock, startDate, endDate){
+	var dataArray = [];
+        async function getData()
+        {
+            var dataFromJSON = await fetch("http://apihack-env.eba-v8abcenj.us-east-1.elasticbeanstalk.com/api/stock/" + stock + "/" + startDate + "/" + endDate);
+
+            dataArray = await dataFromJSON.json();
+			dataArray = dataArray.data;
+			
+			valuesArray = Object.values(dataArray);
+			
+			if (valuesArray[valuesArray.length-1] > valuesArray[0]){
+				diff = valuesArray[valuesArray.length-1]/valuesArray[0]
+			} else{
+				diff = valuesArray[0]/valuesArray[valuesArray.length-1] * -1
+			}
+			
+			
+			return diff*100;
+        }
+		return getData();
+}
+
+function drawChart(stock, startDate, endDate){
+	var dataArray = [];
+        async function getData()
+        {
+            var dataFromJSON = await fetch("http://apihack-env.eba-v8abcenj.us-east-1.elasticbeanstalk.com/api/stock/" + stock + "/" + startDate + "/" + endDate);
+
+            dataArray = await dataFromJSON.json();
+			dataArray = dataArray.data;
+			console.log(Object.values(dataArray));
+
+            ctx = document.getElementById('barChart');
+                new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: Object.keys(dataArray),
+                    datasets: [{
+                    label: '',
+                    data: Object.values(dataArray),
+                    borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                    y: {
+                        beginAtZero: true
+                        }
+                    }
+                }
+            })
+        }
+		getData();
+}
